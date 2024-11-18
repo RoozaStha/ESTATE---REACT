8beaa5a12f605544105e9d53d4d9f0a128d3f239
Roja Shrestha <stharooza3@gmail.com>
2024-11-18 18:08:07 +0545

    almost done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            <div>
+               <img src={assets.logo_dark} alt="" />
+               
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
